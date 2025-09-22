716ea974e2c8c05cabcf48917052c08056500612
Gabi-mis <kx410tklymipene@gmail.com>
2025-09-22 08:18:11 +0100

    adivina

---
+++ adivina.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+num=$((RANDOM % 100 + 1))
+echo "adivina en 5 intentos un numero aleatorio del 1 al 100"
+for i in {1..5}; do
+  read -p "intento $i: " x
+  [ "$x" -eq "$num" ] && echo "🎉 ¡enhorabuena adivinaste en $i intentos!" && exit
+  [ "$x" -lt "$num" ] && echo "el número es MAYOR" || echo "wl número es MENOR"
+done
+echo "❌ Sin intentos. el número era: $num"
